9648d19880b47720f9364e5fd3f114999a7f3d6d
amjed2001 <99887680+amjed2001@users.noreply.github.com>
2022-03-20 23:59:15 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+from textblob import TextBlob
+
+text = ''' gladly i am here in INPT'''
+
+blob = TextBlob(text)
+blob.tags           # [('The', 'DT'), ('titular', 'JJ'),
+                    #  ('threat', 'NN'), ('of', 'IN'), ...]
+
+blob.noun_phrases   # WordList(['titular threat', 'blob',
+                    #            'ultimate movie monster',
+                    #            'amoeba-like mass', ...])
+
+for sentence in blob.sentences:
+    print(sentence.sentiment.polarity)
+# 0.060
